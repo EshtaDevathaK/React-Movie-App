3facd70091db469a4b6e6a6550c138c24e2728c7
eshta <eshtadevatha06@gmail.com>
2025-03-15 19:57:57 +0530

    Initial Commit-3

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-    port: process.env.PORT || 3000 // Use Render's PORT variable
+    port: process.env.PORT || 3000, // Ensure Render uses its assigned PORT
+    strictPort: true // Enforce the exact port
   },
   preview: {
     allowedHosts: ["react-movie-app-ckuv.onrender.com"]
