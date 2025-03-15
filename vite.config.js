875fc67deccfba12509e72e6795eca0627d61ec0
eshta <eshtadevatha06@gmail.com>
2025-03-15 19:48:48 +0530

    Initial Commit-3

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000 // Use Render's PORT variable
+  },
+  preview: {
+    allowedHosts: ["react-movie-app-ckuv.onrender.com"]
+  }
 })
